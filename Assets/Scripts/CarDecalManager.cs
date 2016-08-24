b7f60506412cd4f161d994616ec61a18c1dc0ccd
Magic <kelott5566@gmail.com>
2016-08-24 14:26:39 -0600

    Beginning the car wrap options and stuff

---
+++ Assets/Scripts/CarDecalManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarDecalManager : MonoBehaviour {
+
+    public Material Wrap {
+        get {
+            return renderer.enabled ? renderer.material : null;
+        }
+        set {
+            if (value == null) {
+                renderer.enabled = false;
+            } else {
+                renderer.material = value;
+                renderer.enabled = true;
+            }
+        }
+    }
+
+    private Renderer _renderer;
+    private new Renderer renderer {
+        get {
+            return _renderer != null ? _renderer : (_renderer = GetComponent<Renderer>());
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/CarWrap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+[Serializable]
+public class CarWrap {
+    public string WrapName;
+    public List<CarDecal> Decals;
+}
+
+[Serializable]
+public class CarDecal {
+    public string Texture;
+    public Vector2 Position;
+    public Vector2 Scale;
+    public float Rotation;
+    public CarDecalPart Part;
+}
+
+public enum CarDecalPart {
+    Hood = 0, Rear, Top, Left, Right
+}
